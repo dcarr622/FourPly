31b3aee5536629a72d98ffac377e6b2f9b98d696
James Carr <jcarr@berkeley.edu>
2016-01-23 03:49:16 -0500

    Add chat button to BathroomActivity, start handling parse data for BathroomActivities

--- app/src/main/java/perihelion/io/fourply/BathroomActivity.java
+++ app/src/main/java/perihelion/io/fourply/BathroomActivity.java
@@ -1,19 +1,33 @@
 package perihelion.io.fourply;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.parse.GetCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseInstallation;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.squareup.picasso.Picasso;
 
+import perihelion.io.fourply.chat.ChatActivity;
+import perihelion.io.fourply.data.Bathroom;
+
 public class BathroomActivity extends AppCompatActivity {
+
+    String bathroomName;
+    String bathroomID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +36,20 @@ public class BathroomActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Parse.enableLocalDatastore(this);
-        ParseUser.enableAutomaticUser();
-        ParseInstallation.getCurrentInstallation().saveInBackground();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            bathroomName = extras.getString("name");
+            bathroomID = extras.getString("id");
+        }
+
+
+
+        CollapsingToolbarLayout layout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        layout.setTitle(bathroomName);
+
+//        Parse.enableLocalDatastore(this);
+//        ParseUser.enableAutomaticUser();
+//        ParseInstallation.getCurrentInstallation().saveInBackground();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.bathroomfab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -35,7 +60,41 @@ public class BathroomActivity extends AppCompatActivity {
             }
         });
 
-        ImageView hero = (ImageView) findViewById(R.id.bathroom_hero);
-        Picasso.with(this).load("http://www.adsbeat.com/wp-content/uploads/2014/06/bathroom-fantastic-nice-bathroom-ideas-with-amazing-black-subway-tile-stone-and-brown-wooden-cabinet-also-white-marble-sink-combine-with-white-pendant-lamps-for-bathroom-design-ideas-creative-and-inno.jpg").into(hero);
+        ParseQuery<Bathroom> bathrooms = ParseQuery.getQuery(Bathroom.class);
+        bathrooms.getInBackground(bathroomID, new GetCallback<Bathroom>() {
+            @Override
+            public void done(Bathroom object, ParseException e) {
+                ImageView hero = (ImageView) findViewById(R.id.bathroom_hero);
+                //Picasso.with(BathroomActivity.this).load(object.getHeroImage()).into(hero);
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_bathroom, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.chatbutton:
+                Intent chatIntent = new Intent(this, ChatActivity.class);
+                chatIntent.putExtra("id", bathroomID);
+                chatIntent.putExtra("name", bathroomName);
+                startActivity(chatIntent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
