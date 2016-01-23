9610919f2c3ac12e8dc66588e3d4c8ebc859e1de
David Carr <dcarr@scienceprousa.com>
2016-01-23 18:21:41 -0500

    nearby header

--- app/src/main/java/perihelion/io/fourply/nearby/MainActivity.java
+++ app/src/main/java/perihelion/io/fourply/nearby/MainActivity.java
@@ -62,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.bathroom_list);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewHeader header = RecyclerViewHeader.fromXml(this, R.nearby_header);
+        header.attachTo(recyclerView);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
