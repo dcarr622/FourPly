e5b85d5bd22124f94100bb451249484c343acbd1
Vinnie Magro <vmagro@fb.com>
2017-07-13 00:14:52 -0700

    Add OpenCV sdk to allow compiling native library

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include ../../../../../OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+include $(LOCAL_PATH)/../../../../OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := argraffiti
 LOCAL_SRC_FILES := argraffiti.cpp
-LOCAL_LDLIBS +=  -llog -ldl
+LOCAL_LDLIBS +=  -llog -ldl -latomic
 LOCAL_CFLAGS := -std=c++11
 
 include $(BUILD_SHARED_LIBRARY)
